aa0d8a34dc8b11044a8f34e2ab4afa0f6a58e085
Vijay Menon <vsm@google.com>
2016-09-09 14:41:52 -0700

    Minor fix

--- pkg/dev_compiler/karma.conf.js
+++ pkg/dev_compiler/karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
       },
     },
 
-    browsers: ['Firefox'],
+    browsers: ['Chrome', 'Firefox'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
